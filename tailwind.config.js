71ca6fda757abe3299899be5580d897753085e0b
Mateus <mateuscorreiaazevedo@gmail.com>
2023-05-27 13:23:47 -0300

    feat: init application

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1440px',
+        '3xl': '2520px'
       },
+      padding: {
+        DEFAULT: '2.5rem',
+        '2xl': '5rem'
+      }
     },
+    extend: {
+      scale: {
+        input: '503px'
+      },
+      fontSize: {
+        '2xs': '0.625rem'
+      },
+      colors: {
+        rose: {
+          500: '#FF385C'
+        },
+        pink: {
+          500: '#D70565'
+        }
+      }
+    }
   },
   plugins: [],
 }
